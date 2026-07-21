2fb26c19e5ca22afa8a71f2edc7237d755338353
Ivan <ivanbabaev217@gmail.com>
2026-07-21 17:35:00 +0800

    window_create() now creates a window with a vulkan renderer

--- engine/src/core/window.c
+++ engine/src/core/window.c
@@ -14,9 +14,17 @@ Window *window_create(const char *title, int w, int h)
 {
     Window *window = malloc(sizeof(Window));
 
-    if (!SDL_CreateWindowAndRenderer(title, w, h, SDL_WINDOW_VULKAN | SDL_WINDOW_HIDDEN, &window->ptr_sdl_window, &window->ptr_sdl_renderer))
+    window->ptr_sdl_window = SDL_CreateWindow(title, w, h, SDL_WINDOW_VULKAN | SDL_WINDOW_HIDDEN);
+    if (window->ptr_sdl_window == NULL)
     {
-        SDL_Log("Failed to create window or renderer: %s", SDL_GetError());
+        SDL_Log("Failed to create native window: %s", SDL_GetError());
+        return NULL;
+    }
+
+    window->ptr_sdl_renderer = SDL_CreateRenderer(window->ptr_sdl_window, "vulkan");
+    if (window->ptr_sdl_renderer == NULL)
+    {
+        SDL_Log("Failed to create renderer: %s", SDL_GetError());
         return NULL;
     }
 
