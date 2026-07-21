1e02f964f1897e258db59bf3adbced88c916ec60
Ivan <ivanbabaev217@gmail.com>
2026-07-21 22:39:16 +0800

    Update dackel2d.h

--- engine/include/dackel2d.h
+++ engine/include/dackel2d.h
@@ -1,8 +1,16 @@
 #pragma once
 
 #include "core/window.h"
-#include "core/events.h"
 #include "core/application.h"
+
+#include "input/key_code.h"
+#include "input/mouse_button.h"
+
+#include "events/event.h"
+#include "events/event_key.h"
+#include "events/event_mouse_button.h"
+#include "events/event_mouse_wheel.h"
+#include "events/event_mouse_motion.h"
 
 #include "input/key_code.h"
 #include "input/mouse_button.h"
