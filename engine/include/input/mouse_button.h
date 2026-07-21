6e41ce413aa54e96fca2caa9e1bceb37f0aa4926
Ivan <ivanbabaev217@gmail.com>
2026-07-21 21:31:52 +0800

    Create mouse_button.h

---
+++ engine/include/input/mouse_button.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <SDL3/SDL.h>
+
+enum MouseButton
+{
+    Left = SDL_BUTTON_LEFT,
+    Middle = SDL_BUTTON_MIDDLE,
+    Right = SDL_BUTTON_RIGHT,
+    Side1 = SDL_BUTTON_X1,
+    Side2 = SDL_BUTTON_X2,
+};
