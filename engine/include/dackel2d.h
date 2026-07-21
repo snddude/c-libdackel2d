ff6629244ae9a0e5ebb005da593325931e03d077
Ivan <ivanbabaev217@gmail.com>
2026-07-21 21:54:55 +0800

    Update dackel2d.h

--- engine/include/dackel2d.h
+++ engine/include/dackel2d.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "core/window.h"
+#include "core/events.h"
 #include "core/application.h"
+
+#include "input/key_code.h"
+#include "input/mouse_button.h"
 
 #include "math/vector2.h"
