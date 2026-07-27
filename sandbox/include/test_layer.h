82441fddc236bf26b5cdf8f66c3a9a3a2a3ccc09
Ivan <ivanbabaev217@gmail.com>
2026-07-27 13:56:23 +0800

    Implement test layer in sandbox

---
+++ sandbox/include/test_layer.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <dackel2d.h>
+
+Layer test_layer_create();
+void test_layer_destroy(Layer *layer);
---
+++ sandbox/src/test_layer.c
@@ -0,0 +1,54 @@
+#include "test_layer.h"
+
+#include <dackel2d.h>
+#include <stdlib.h>
+
+static void on_attach()
+{
+    SDL_Log("Hello from test layer!");
+}
+
+static void on_detach()
+{
+    SDL_Log("Goodbye from test layer!");
+}
+
+static bool process_event(Event event)
+{
+    switch (event.type)
+    {
+        case EventType_MouseMotion:
+            EventMouseMotion *mm = (EventMouseMotion *)event.super_type;
+            SDL_Log("[INFO] x: %.2f y: %.2f", mm->absolute.x, mm->absolute.y);
+            break;
+        case EventType_Key:
+            EventKey *k = (EventKey *)event.super_type;
+
+            if (!k->echo && k->pressed)
+                SDL_Log(k->label);
+    }
+}
+
+static void process(double delta)
+{
+    SDL_Log("[INFO] delta time: %f", delta);
+}
+
+static void render(SDL_Renderer *renderer)
+{
+    SDL_FRect rect = {8.0f, 8.0f, 64.0f, 64.0f};
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderFillRect(renderer, &rect);
+}
+
+Layer test_layer_create()
+{
+    return (Layer){
+        .on_attach = on_attach,
+        .on_detach = on_detach,
+        .process_event = process_event,
+        .process = process,
+        .render = render
+    };
+}
