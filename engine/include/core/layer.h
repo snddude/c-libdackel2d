75999cd0c2fb46fb46a0c1db56369cc31f0af913
Ivan <ivanbabaev217@gmail.com>
2026-07-27 13:52:34 +0800

    Change Layer api
    - layer_ functions now take in a layer by value.

--- engine/include/core/layer.h
+++ engine/include/core/layer.h
@@ -14,9 +14,9 @@ typedef struct layer
 
 } Layer;
 
-void layer_on_attach(Layer *layer);
-void layer_on_detach(Layer *layer);
+void layer_on_attach(Layer layer);
+void layer_on_detach(Layer layer);
 
-bool layer_process_event(Layer *layer, Event event);
-void layer_process(Layer *layer, double delta);
-void layer_render(Layer *layer, SDL_Renderer *renderer);
+bool layer_process_event(Layer layer, Event event);
+void layer_process(Layer layer, double delta);
+void layer_render(Layer layer, SDL_Renderer *renderer);
--- engine/src/core/layer.c
+++ engine/src/core/layer.c
@@ -3,34 +3,34 @@
 #include <SDL3/SDL.h>
 #include "events/event.h"
 
-void layer_on_attach(Layer *layer)
+void layer_on_attach(Layer layer)
 {
-    if (layer->on_attach != NULL)
-        layer->on_attach();
+    if (layer.on_attach != NULL)
+        layer.on_attach();
 }
 
-void layer_on_detach(Layer *layer)
+void layer_on_detach(Layer layer)
 {
-    if (layer->on_detach != NULL)
-        layer->on_detach();
+    if (layer.on_detach != NULL)
+        layer.on_detach();
 }
 
-bool layer_process_event(Layer *layer, Event event)
+bool layer_process_event(Layer layer, Event event)
 {
-    if (layer->process_event != NULL)
-        return layer->process_event(event);
+    if (layer.process_event != NULL)
+        return layer.process_event(event);
 
     return false;
 }
 
-void layer_process(Layer *layer, double delta)
+void layer_process(Layer layer, double delta)
 {
-    if (layer->process != NULL)
-        layer->process(delta);
+    if (layer.process != NULL)
+        layer.process(delta);
 }
 
-void layer_render(Layer *layer, SDL_Renderer *renderer)
+void layer_render(Layer layer, SDL_Renderer *renderer)
 {
-    if (layer->render != NULL)
-        layer->render(renderer);
+    if (layer.render != NULL)
+        layer.render(renderer);
 }
