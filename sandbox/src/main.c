02be28e318bbae1e79ccaedf9f2c6b760248e7f1
Ivan <ivanbabaev217@gmail.com>
2026-07-27 13:56:30 +0800

    Update main.c

--- sandbox/src/main.c
+++ sandbox/src/main.c
@@ -1,11 +1,16 @@
 #include <dackel2d.h>
 
+#include "test_layer.h"
+
 int main(void)
 {
     Application *app = application_create();
 
     if (app == NULL)
         return 1;
+
+    Layer test_layer = test_layer_create();
+    application_push_layer(app, test_layer);
 
     application_run(app);
     application_destroy(app);
