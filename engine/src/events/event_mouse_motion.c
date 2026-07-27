01275671eb7a1bf2849b8041303e2e62321676e4
Ivan <ivanbabaev217@gmail.com>
2026-07-27 13:51:07 +0800

    Fix event_mouse_motion_create() not assign  the y component

--- engine/src/events/event_mouse_motion.c
+++ engine/src/events/event_mouse_motion.c
@@ -6,7 +6,13 @@
 EventMouseMotion event_mouse_motion_create(SDL_Event event)
 {
     return (EventMouseMotion){
-        .absolute = (Vector2){.x = event.motion.x, .x = event.motion.y},
-        .relative = (Vector2){.x = event.motion.xrel, .x = event.motion.yrel}
+        .absolute = (Vector2){
+            .x = event.motion.x,
+            .y = event.motion.y
+        },
+        .relative = (Vector2){
+            .x = event.motion.xrel,
+            .y = event.motion.yrel
+        }
     };
 }
