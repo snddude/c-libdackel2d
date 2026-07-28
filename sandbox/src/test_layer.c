f0e51cde478123bd1f729e0f8b33bb58af8c2e33
Ivan <ivanbabaev217@gmail.com>
2026-07-28 18:44:33 +0800

    Update test_layer.c

--- sandbox/src/test_layer.c
+++ sandbox/src/test_layer.c
@@ -3,6 +3,10 @@
 #include <dackel2d.h>
 #include <stdlib.h>
 
+#define SPEED 128.0f
+
+static Vector2 wish_vel;
+
 static void on_attach()
 {
     SDL_Log("Hello from test layer!");
@@ -36,9 +40,23 @@ static void process_event(Event *event)
     }
 }
 
+static void process(double delta)
+{
+    Vector2 input = input_get_vector(KeyCode_A, KeyCode_D, KeyCode_S, KeyCode_W);
+    vector2_normalize(&input);
+    Vector2 velocity = vector2_multiply(input, delta * SPEED);
+
+    wish_vel = vector2_add(wish_vel, velocity);
+}
+
 static void render(SDL_Renderer *renderer)
 {
-    SDL_FRect rect = {8.0f, 8.0f, 64.0f, 64.0f};
+    SDL_FRect rect = {
+        0.0f + wish_vel.x,
+        0.0f + wish_vel.y,
+        16.0f,
+        16.0f
+    };
 
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
     SDL_RenderFillRect(renderer, &rect);
@@ -50,7 +68,7 @@ Layer test_layer_create()
         .on_attach = on_attach,
         .on_detach = on_detach,
         .process_event = process_event,
-        .process = NULL,
+        .process = process,
         .render = render
     };
 }
