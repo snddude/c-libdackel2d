54ce2df00c9785b1e1d380cc575fd094b663f73c
Ivan <ivanbabaev217@gmail.com>
2026-07-31 19:14:04 +0800

    Rename Event's super_type to sub_event

--- engine/src/events/event.c
+++ engine/src/events/event.c
@@ -13,26 +13,26 @@ Event event_create(SDL_Event native_event)
     {
         case SDL_EVENT_KEY_DOWN: case SDL_EVENT_KEY_UP:
             EventKey key = event_key_create(native_event);
-            event.super_type = (void *)&key;
+            event.sub_event = (void *)&key;
             event.type = EventType_Key;
             break;
         case SDL_EVENT_MOUSE_BUTTON_DOWN: case SDL_EVENT_MOUSE_BUTTON_UP:
             EventMouseButton mb = event_mouse_button_create(native_event);
-            event.super_type = (void *)&mb;
+            event.sub_event = (void *)&mb;
             event.type = EventType_MouseButton;
             break;
         case SDL_EVENT_MOUSE_WHEEL:
             EventMouseWheel mw = event_mouse_wheel_create(native_event);
-            event.super_type = (void *)&mw;
+            event.sub_event = (void *)&mw;
             event.type = EventType_MouseWheel;
             break;
         case SDL_EVENT_MOUSE_MOTION:
             EventMouseMotion mm = event_mouse_motion_create(native_event);
-            event.super_type = (void *)&mm;
+            event.sub_event = (void *)&mm;
             event.type = EventType_MouseMotion;
             break;
         default:
-            event.super_type = NULL;
+            event.sub_event = NULL;
             event.type = EventType_Unhandled;
             break;
     }
