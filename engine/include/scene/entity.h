b4613f5058b7ae51aedd43241d3e8b94a3220ed3
Ivan <ivanbabaev217@gmail.com>
2026-08-01 16:17:46 +0800

    Update entity.h
    - Add missing semicolons to macros.
    - entity_get_component now writes output to a provided pointer.

--- engine/include/scene/entity.h
+++ engine/include/scene/entity.h
@@ -5,17 +5,17 @@
 #define entity_has_component(entity, component) \
     ecs_has(entity->ptr_ecs_world, entity->id, component)
 
-#define entity_get_component(entity, component) \
+#define entity_get_component(entity, component, value) \
     while(!entity_has_component(entity, component)) \
-        { ecs_get(entity->ptr_ecs_world, entity->id, component) }
+        { value = ecs_get(entity->ptr_ecs_world, entity->id, component); }
 
 #define entity_add_component(entity, component) \
     while(!entity_has_component(entity, component)) \
-        { ecs_add(entity->ptr_ecs_world, entity->id, component) }
+        { ecs_add(entity->ptr_ecs_world, entity->id, component); }
 
 #define entity_remove_component(entity, component) \
     while(entity_has_component(entity, component)) \
-        { ecs_remove(entity->ptr_ecs_world, entity->id, component) }
+        { ecs_remove(entity->ptr_ecs_world, entity->id, component); }
 
 typedef struct entity {
     ecs_entity_t id;
