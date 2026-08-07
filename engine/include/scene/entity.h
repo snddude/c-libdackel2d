51dbb255acba18064b3588cc3f318cf9d379a97a
Ivan <ivanbabaev217@gmail.com>
2026-08-08 01:31:14 +0800

    Update entity.h
    - Remove parentheses from around the component argument of the macros.

--- engine/include/scene/entity.h
+++ engine/include/scene/entity.h
@@ -3,18 +3,18 @@
 #include <flecs.h>
 
 #define entity_has_component(self, component) \
-    ecs_has((self)->ecs_world_p, (self)->id, (component))
+    ecs_has((self)->ecs_world_p, (self)->id, component)
 
 #define entity_get_component(self, component) \
-    ecs_get((self)->ecs_world_p, (self)->id, (component))
+    ecs_get((self)->ecs_world_p, (self)->id, component)
 
 #define entity_add_component(self, component) \
-    while(!entity_has_component((self), (component))) \
-        { ecs_add((self)->ecs_world_p, (self)->id, (component)); }
+    while(!entity_has_component((self), component)) \
+        { ecs_add((self)->ecs_world_p, (self)->id, component); }
 
 #define entity_remove_component(self, component) \
-    while(entity_has_component((self), (component))) \
-        { ecs_remove((self)->ecs_world_p, (self)->id, (component)); }
+    while(entity_has_component((self), component)) \
+        { ecs_remove((self)->ecs_world_p, (self)->id, component); }
 
 typedef struct 
 {
