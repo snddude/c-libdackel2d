19f31f86d8038f4617864c84afbb1b09bcff5a6d
Ivan <ivanbabaev217@gmail.com>
2026-08-07 18:24:32 +0800

    Rename MouseButton to mouse_button_t

--- engine/include/input/mouse_button.h
+++ engine/include/input/mouse_button.h
@@ -2,12 +2,11 @@
 
 #include <SDL3/SDL.h>
 
-typedef enum mouse_button
+typedef enum
 {
     MouseButton_Left = SDL_BUTTON_LEFT,
     MouseButton_Middle = SDL_BUTTON_MIDDLE,
     MouseButton_Right = SDL_BUTTON_RIGHT,
     MouseButton_Side1 = SDL_BUTTON_X1,
     MouseButton_Side2 = SDL_BUTTON_X2,
-
-} MouseButton;
+} mouse_button_t;
