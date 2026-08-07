c9fc916b65cfb594e893f23b3a3d669c89a5d45d
Ivan <ivanbabaev217@gmail.com>
2026-08-07 18:25:11 +0800

    Update layer.h

--- engine/include/core/layer.h
+++ engine/include/core/layer.h
@@ -8,7 +8,7 @@ typedef struct
 	void (*on_attach)();
 	void (*on_detach)();
 
-	void (*process_event)(Event *event);
+	void (*process_event)(event_t *event);
 	void (*process)(double delta);
 	void (*render)(SDL_Renderer *renderer);
 
@@ -17,6 +17,6 @@ typedef struct
 void layer_on_attach(layer_t *self);
 void layer_on_detach(layer_t *self);
 
-void layer_process_event(layer_t *self, Event *event);
+void layer_process_event(layer_t *self, event_t *event);
 void layer_process(layer_t *self, double delta);
 void layer_render(layer_t *self, SDL_Renderer *renderer);
