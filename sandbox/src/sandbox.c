d68a8e3141af56fbf3a8f93743514c5a8417b2a5
Ivan <ivanbabaev217@gmail.com>
2026-08-07 16:26:10 +0800

    Update sandbox.c

--- sandbox/src/sandbox.c
+++ sandbox/src/sandbox.c
@@ -3,16 +3,16 @@
 #include <dackel2d.h>
 #include "test_layer.h"
 
-Application *sandbox_create()
+application_t sandbox_create()
 {
-    Application *app = application_create();
-    Window *window = application_get_main_window(app);
+    application_t app = application_create();
+    Window *window = application_get_main_window(&app);
 
     window_set_title(window, "Sandbox");
     window_set_size(window, 1280, 800);
 
     Layer test_layer = test_layer_create();
-    application_push_layer(app, test_layer);
+    application_push_layer(&app, test_layer);
 
     return app;
 }
