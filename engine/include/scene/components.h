9c49e1bf731e3df2dd7d45598bb2ec2feb566cd0
Ivan <ivanbabaev217@gmail.com>
2026-08-08 00:50:24 +0800

    Rename all components to new convention

--- engine/include/scene/components.h
+++ engine/include/scene/components.h
@@ -4,46 +4,42 @@
 
 #include <flecs.h>
 #include <SDL3/SDL.h>
+#include <stdbool.h>
 
-typedef struct name_tag
+typedef struct
 {
     const char *name;
+} name_tag_t;
 
-} NameTag;
-
-typedef struct transform
+typedef struct
 {
     float rotation;
     Vector2 position;
     Vector2 scale;
+} transform_t;
 
-} Transform;
-
-typedef struct renderable
+typedef struct
 {
     int z_index;
     bool visible;
+} renderable_t;
 
-} Renderable;
-
-typedef struct colored_rect
+typedef struct
 {
     Vector2 size;
     SDL_FColor color;
+} colored_rect_t;
 
-} ColoredRect;
-
-typedef struct sprite
+typedef struct
 {
     SDL_Texture *texture;
     SDL_FColor modulate;
+} sprite_t;
 
-} Sprite;
-
-extern ECS_COMPONENT_DECLARE(NameTag);
-extern ECS_COMPONENT_DECLARE(Transform);
-extern ECS_COMPONENT_DECLARE(Renderable);
-extern ECS_COMPONENT_DECLARE(ColoredRect);
-extern ECS_COMPONENT_DECLARE(Sprite);
+extern ECS_COMPONENT_DECLARE(name_tag_t);
+extern ECS_COMPONENT_DECLARE(transform_t);
+extern ECS_COMPONENT_DECLARE(renderable_t);
+extern ECS_COMPONENT_DECLARE(colored_rect_t);
+extern ECS_COMPONENT_DECLARE(sprite_t);
 
 void register_components(ecs_world_t *ecs_world);
--- engine/src/scene/components.c
+++ engine/src/scene/components.c
@@ -1,18 +1,16 @@
 #include "scene/components.h"
 
-#include <flecs.h>
-
-ECS_COMPONENT_DECLARE(NameTag);
-ECS_COMPONENT_DECLARE(Transform);
-ECS_COMPONENT_DECLARE(Renderable);
-ECS_COMPONENT_DECLARE(ColoredRect);
-ECS_COMPONENT_DECLARE(Sprite);
+ECS_COMPONENT_DECLARE(name_tag_t);
+ECS_COMPONENT_DECLARE(transform_t);
+ECS_COMPONENT_DECLARE(renderable_t);
+ECS_COMPONENT_DECLARE(colored_rect_t);
+ECS_COMPONENT_DECLARE(sprite_t);
 
 void register_components(ecs_world_t *ecs_world)
 {
-    ECS_COMPONENT_DEFINE(ecs_world, NameTag);
-    ECS_COMPONENT_DEFINE(ecs_world, Transform);
-    ECS_COMPONENT_DEFINE(ecs_world, Renderable);
-    ECS_COMPONENT_DEFINE(ecs_world, ColoredRect);
-    ECS_COMPONENT_DEFINE(ecs_world, Sprite);
+    ECS_COMPONENT_DEFINE(ecs_world, name_tag_t);
+    ECS_COMPONENT_DEFINE(ecs_world, transform_t);
+    ECS_COMPONENT_DEFINE(ecs_world, renderable_t);
+    ECS_COMPONENT_DEFINE(ecs_world, colored_rect_t);
+    ECS_COMPONENT_DEFINE(ecs_world, sprite_t);
 }
