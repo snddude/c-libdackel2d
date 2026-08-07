a6ab2fc4f7d12b2717170e5442c0695390c26d46
Ivan <ivanbabaev217@gmail.com>
2026-08-07 17:04:57 +0800

    Update application.h
    - Remove application_get_main_window method.

--- engine/include/core/application.h
+++ engine/include/core/application.h
@@ -5,9 +5,8 @@
 
 typedef struct application
 {
-    Window *ptr_main_window;
+    window_t main_window;
     Layer *layer_stack;
-
 } application_t;
 
 application_t application_create();
@@ -18,5 +17,3 @@ void application_run(application_t *self);
 void application_push_layer(application_t *self, Layer layer);
 void application_pop_layer(application_t *self);
 void application_pop_layer_at(application_t *self, size_t index);
-
-Window *application_get_main_window(application_t *self);
