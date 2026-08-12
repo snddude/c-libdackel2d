06bdaccbcab5458eb1cce4fb0e8f6cdfe9c6c46f
Ivan <ivanbabaev217@gmail.com>
2026-08-12 21:57:00 +0800

    Create colors.h

---
+++ engine/include/logger/colors.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#define FG_BLACK   "\e[0;30m"
+#define FG_RED     "\e[0;31m"
+#define FG_GREEN   "\e[0;32m"
+#define FG_YELLOW  "\e[0;33m"
+#define FG_BLUE    "\e[0;34m"
+#define FG_MAGENTA "\e[0;35m"
+#define FG_CYAN    "\e[0;36m"
+#define FG_WHITE   "\e[0;37m"
+
+#define BG_BLACK   "\e[40m"
+#define BG_RED     "\e[41m"
+#define BG_GREEN   "\e[42m"
+#define BG_YELLOW  "\e[43m"
+#define BG_BLUE    "\e[44m"
+#define BG_MAGENTA "\e[45m"
+#define BG_CYAN    "\e[46m"
+#define BG_WHITE   "\e[47m"
+
+#define END "\e[0m"
