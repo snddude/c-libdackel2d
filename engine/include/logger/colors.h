c802f7f6934febd149b4698d75f582c1b7e2869b
Ivan <ivanbabaev217@gmail.com>
2026-08-12 23:41:54 +0800

    Add bold foreground colors

--- engine/include/logger/colors.h
+++ engine/include/logger/colors.h
@@ -9,6 +9,15 @@
 #define FG_CYAN    "\e[0;36m"
 #define FG_WHITE   "\e[0;37m"
 
+#define BFG_BLACK   "\e[1;30m"
+#define BFG_RED     "\e[1;31m"
+#define BFG_GREEN   "\e[1;32m"
+#define BFG_YELLOW  "\e[1;33m"
+#define BFG_BLUE    "\e[1;34m"
+#define BFG_MAGENTA "\e[1;35m"
+#define BFG_CYAN    "\e[1;36m"
+#define BFG_WHITE   "\e[1;37m"
+
 #define BG_BLACK   "\e[40m"
 #define BG_RED     "\e[41m"
 #define BG_GREEN   "\e[42m"
