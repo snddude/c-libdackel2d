caf4dcbe96ed1ed659d8b6ad52b61000229f0173
Ivan <ivanbabaev217@gmail.com>
2026-08-12 23:42:39 +0800

    Changes to logging macros
    - Add get_timestamp function.
    - Improve message format.

--- engine/include/logger/logger.h
+++ engine/include/logger/logger.h
@@ -4,6 +4,8 @@
 
 #include <stdio.h>
 
-#define log_info(fmt, ...) fprintf(stdout, "%sINFO: "fmt"%s\n", FG_WHITE, __VA_ARGS__, END)
-#define log_error(fmt, ...) fprintf(stderr, "%sERROR: "fmt"%s\n", FG_RED, __VA_ARGS__, END)
-#define log_warning(fmt, ...) fprintf(stderr, "%sWARNING: "fmt"%s\n", FG_YELLOW, __VA_ARGS__, END)
+#define log_info(fmt, ...) fprintf(stdout, "[%s] [Dackel2D] [%sinfo%s] [%s:%d]: "fmt"\n", get_timestamp(), BFG_MAGENTA, END, __FILE_NAME__, __LINE__, ##__VA_ARGS__)
+#define log_error(fmt, ...) fprintf(stderr, "[%s] [Dackel2D] [%serror%s] [%s:%d]: "fmt"\n", get_timestamp(), BFG_RED, END, __FILE_NAME__, __LINE__, ##__VA_ARGS__)
+#define log_warning(fmt, ...) fprintf(stderr, "[%s] [Dackel2D] [%swarning%s] [%s:%d]: "fmt"\n", get_timestamp(), BFG_YELLOW, END , __FILE_NAME__, __LINE__, ##__VA_ARGS__)
+
+const char *get_timestamp();
