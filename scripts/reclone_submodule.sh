1cc13186e2e9250d2aa528fa1897c5cd7449b8fa
Ivan <ivanbabaev217@gmail.com>
2026-08-30 18:07:27 +0800

    Update reclone_submodule.sh

--- scripts/reclone_submodule.sh
+++ scripts/reclone_submodule.sh
@@ -1,9 +1,9 @@
 submodule=""
 read -p "Enter submodule name: " submodule
 
-git submodule deinit -f engine/vendor/$submodule
+git submodule deinit -f vendor/$submodule
 
-rm -rf .git/modules/engine/vendor/$submodule
-rm -rf engine/vendor/$submodule
+rm -rf .git/modules/vendor/$submodule
+rm -rf vendor/$submodule
 
-git submodule update --init engine/vendor/$submodule
+git submodule update --init vendor/$submodule
