0aac3faf5ea16b81b44dfb9371a33b44cf4a44cd
Ivan <ivanbabaev217@gmail.com>
2026-09-20 19:46:59 +0800

    Change mouse_button_t enum naming convention

--- include/input/mouse_button.h
+++ include/input/mouse_button.h
@@ -4,9 +4,9 @@
 
 typedef enum
 {
-    MouseButton_Left = SDL_BUTTON_LEFT,
-    MouseButton_Middle = SDL_BUTTON_MIDDLE,
-    MouseButton_Right = SDL_BUTTON_RIGHT,
-    MouseButton_Side1 = SDL_BUTTON_X1,
-    MouseButton_Side2 = SDL_BUTTON_X2,
+    MOUSE_BUTTON_LEFT   = SDL_BUTTON_LEFT,
+    MOUSE_BUTTON_MIDDLE = SDL_BUTTON_MIDDLE,
+    MOUSE_BUTTON_RIGHT  = SDL_BUTTON_RIGHT,
+    MOUSE_BUTTON_SIDE1  = SDL_BUTTON_X1,
+    MOUSE_BUTTON_SIDE2  = SDL_BUTTON_X2,
 } mouse_button_t;
