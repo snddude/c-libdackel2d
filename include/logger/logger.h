03b477ebed7b7ec9fce5e535683285055947dec7
Ivan <ivanbabaev217@gmail.com>
2026-09-20 19:34:27 +0800

    store_message now also takes into account the current log level

--- include/logger/logger.h
+++ include/logger/logger.h
@@ -8,9 +8,9 @@
 #define log_error(fmt, ...) log_message(stdout, LOG_LEVEL_ERROR, "error", __FILE_NAME__, __LINE__, BFG_RED, fmt, ##__VA_ARGS__)
 #define log_warning(fmt, ...) log_message(stdout, LOG_LEVEL_WARNING, "warning", __FILE_NAME__, __LINE__, BFG_YELLOW, fmt, ##__VA_ARGS__)
 
-#define slog_info(fmt, ...) { log_info(fmt, ##__VA_ARGS__); store_message("info", __FILE_NAME__, __LINE__, fmt, ##__VA_ARGS__); }
-#define slog_error(fmt, ...) { log_error(fmt, ##__VA_ARGS__); store_message("error", __FILE_NAME__, __LINE__, fmt, ##__VA_ARGS__); }
-#define slog_warning(fmt, ...) { log_warning(fmt, ##__VA_ARGS__); store_message("warning", __FILE_NAME__, __LINE__, fmt, ##__VA_ARGS__); }
+#define slog_info(fmt, ...) { log_info(fmt, ##__VA_ARGS__); store_message(LOG_LEVEL_INFO, "info", __FILE_NAME__, __LINE__, fmt, ##__VA_ARGS__); }
+#define slog_error(fmt, ...) { log_error(fmt, ##__VA_ARGS__); store_message(LOG_LEVEL_ERROR, "error", __FILE_NAME__, __LINE__, fmt, ##__VA_ARGS__); }
+#define slog_warning(fmt, ...) { log_warning(fmt, ##__VA_ARGS__); store_message(LOG_LEVEL_WARNING, "warning", __FILE_NAME__, __LINE__, fmt, ##__VA_ARGS__); }
 
 typedef enum
 {
@@ -25,4 +25,4 @@ const char *get_timestamp();
 
 void set_log_level(log_level_t value);
 void log_message(FILE *stream, log_level_t log_level, const char *type, const char *file, int line, const char *color, const char *fmt, ...);
-void store_message(const char *type, const char *file, int line, const char *fmt, ...);
+void store_message(log_level_t log_level, const char *type, const char *file, int line, const char *fmt, ...);
